0e974ef384b529ab98b3dd9448462a16ace9ceca
Daryl Stensland <daryl@Daryls-MacBook-Pro.local>
2022-06-11 15:22:51 +0100

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,33 @@
+exports.getAllUsers = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'Route not yet defined.',
+	});
+};
+
+exports.getSingleUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'Route not yet defined.',
+	});
+};
+exports.updateUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'Route not yet defined.',
+	});
+};
+
+exports.createUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'Route not yet defined.',
+	});
+};
+
+exports.deleteUser = (req, res) => {
+	res.status(500).json({
+		status: 'error',
+		message: 'Route not yet defined.',
+	});
+};
---
+++ routes/tourRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {
+	getAllTours,
+	createTour,
+	getSingleTour,
+	updateTour,
+	deleteTour,
+} = require('../controllers/tourController');
+
+const router = express.Router();
+
+router.route('/').get(getAllTours).post(createTour);
+
+router
+	.route('/:id')
+	.get(getSingleTour)
+	.post(createTour)
+	.patch(updateTour)
+	.delete(deleteTour);
+
+module.exports = router;
