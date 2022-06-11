62de2e925a5fee90cbf8ccc08ec10e68f07eaba4
Daryl Stensland <daryl@Daryls-MacBook-Pro.local>
2022-06-11 16:08:26 +0100

    added middleware to check valid requests

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,29 @@ const tours = JSON.parse(
 	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
+exports.checkID = (req, res, next, val) => {
+	if (val * 1 > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID',
+			requestedAt: req.requestTime,
+		});
+	}
+
+	next();
+};
+
+exports.checkBody = (req, res, next) => {
+	if (!req.body.name || !req.body.price) {
+		return res.status(400).json({
+			status: 'fail',
+			message: 'Invalid properties',
+			requestedAt: req.requestTime,
+		});
+	}
+	next();
+};
+
 exports.getAllTours = (req, res) => {
 	res.status(200).json({
 		status: 'success',
@@ -18,14 +41,6 @@ exports.getAllTours = (req, res) => {
 exports.getSingleTour = (req, res) => {
 	const id = req.params.id * 1;
 	const tour = tours.find((el) => el.id === id);
-
-	if (!tour) {
-		return res.status(404).json({
-			status: 'failed',
-			message: 'Invalid ID',
-			requestedAt: req.requestTime,
-		});
-	}
 
 	res.status(200).json({
 		staus: 'success',
@@ -58,14 +73,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-	if (req.params.id * 1 > tours.length) {
-		return res.status(404).json({
-			status: 'failed',
-			message: 'Invalid ID',
-			requestedAt: req.requestTime,
-		});
-	}
-
 	res.status(200).json({
 		status: 'success',
 		requestedAt: req.requestTime,
@@ -76,13 +83,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-	if (req.params.id * 1 > tours.length) {
-		return res.status(404).json({
-			status: 'failed',
-			message: 'Invalid ID',
-		});
-	}
-
 	res.status(204).json({
 		status: 'success',
 		data: null,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,17 +5,16 @@ const {
 	getSingleTour,
 	updateTour,
 	deleteTour,
+	checkID,
+	checkBody,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.route('/').get(getAllTours).post(createTour);
+router.param('id', checkID);
 
-router
-	.route('/:id')
-	.get(getSingleTour)
-	.post(createTour)
-	.patch(updateTour)
-	.delete(deleteTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
+
+router.route('/:id').get(getSingleTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
