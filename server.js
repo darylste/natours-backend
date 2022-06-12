c254ef7a2e6c2b27153cf8dcbf5cb5252ec4a968
Daryl Stensland <daryl@Daryls-MacBook-Pro.local>
2022-06-12 19:58:58 +0100

    enviroment variables and es-lint setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = 8000;
+dotenv.config({ path: './config.env' });
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-	console.log(`App running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
