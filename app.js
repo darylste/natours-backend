62609f514e2385597935de053e27f45bff4f6b4a
Daryl Stensland <daryl@Daryls-MacBook-Pro.local>
2022-06-20 10:57:57 +0100

    database connected

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ const app = express();
 
 // Middleware
 if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-	console.log('Hello from the middleware!');
-	next();
+  console.log('Hello from the middleware!');
+  next();
 });
 
 app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 // Routes
 app.use('/api/v1/tours', tourRouter);
