ee86ad38f134e6f9bbed6099dc7f948357817412
Daryl Stensland <daryl@Daryls-MacBook-Pro.local>
2022-06-21 16:33:36 +0100

    functionality for querying with greater than / less than params added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,14 +2,23 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Build Query
+    //  1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete query[el]);
+    excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    //  2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
+    console.log(JSON.parse(queryStr));
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // Execute Query
     const tours = await query;
 
+    // Send Response
     res.status(200).json({
       status: 'success',
       results: tours.length,
