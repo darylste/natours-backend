5544895b715cddc409143625fbf4e7e5f4d65a5f
Daryl Stensland <dstensland5@gmail.com>
2022-06-22 10:42:06 +0100

    Field Limiting functionality added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,6 +21,12 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    //  3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else query = query.select('-__v');
+
     // Execute Query
     const tours = await query;
 
