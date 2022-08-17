3450671312dae81318574fcfb9a358df6d6253d3
Daryl Stensland <dstensland5@gmail.com>
2022-08-17 11:25:54 +0100

    handing express cast errors in prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -14,7 +21,7 @@ const sendErrorProd = (err, res) => {
       message: err.message,
     });
   } else {
-    console.error('ERROR', err);
+    console.error('*** ERROR ***', err);
 
     res.status(500).json({
       status: 'error',
@@ -30,6 +37,11 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (err.name == 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    sendErrorProd(error, res);
   }
 };
