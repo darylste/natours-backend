b15995b2fc348abea2626e31ee7564ce79fb6a6b
Daryl Stensland <dstensland5@gmail.com>
2022-08-17 13:45:38 +0100

    uncaught exeption error handling added

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exeption -- shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -25,8 +31,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('Unhandled Rejection -- shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
