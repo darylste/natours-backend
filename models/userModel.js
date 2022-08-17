340d8e6ac165ba11879cd00bee394009d2d14dce
Daryl Stensland <dstensland5@gmail.com>
2022-08-17 15:05:12 +0100

    password hashing via dcrypt added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -33,6 +34,15 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords must match.',
     },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
