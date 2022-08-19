312b187eb4ea2309fa6d859b428728e3f617b543
Daryl Stensland <dstensland5@gmail.com>
2022-08-19 10:40:25 +0100

    updated jwt to be sent as cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,17 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRY * 24 * 60 * 60 * 1000
+    ),
+    secure: process.env.NODE_ENV === 'production' ? true : false,
+    httpOnly: true,
+  };
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
