f2eb842493ad90067b4b7bf627d319e1533b4ebc
Daryl Stensland <dstensland5@gmail.com>
2022-08-22 16:49:08 +0100

    prevent users from submitting multiple reviews for the same tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -41,6 +41,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'user',
