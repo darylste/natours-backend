5ca8704876ae97971343e0645ed29b1884062fed
Daryl Stensland <dstensland5@gmail.com>
2022-08-24 08:35:26 +0100

    tour overview page built

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const { getOverview, getTour } = require('../controllers/viewController');
 const router = express.Router();
 
 router.get('/', getOverview);
-router.get('/tour', getTour);
+router.get('/tour/:tourName', getTour);
 
 module.exports = router;
